4fd2481ab5280541e1460efa1fafcdc8909fe87c
Alberto Miedes <albertomg994@gmail.com>
2019-03-14 17:50:41 +0100

    Parameterize Jenkinsfiles

--- pipelines/budgets_execution/dev_run.sh
+++ pipelines/budgets_execution/dev_run.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 XBRL_FILE="AJ-TrimLoc-20181t.xbrl"
+WORKING_DIR=/tmp/esplugues
+GOBIERTO_ETL_UTILS=$DEV_DIR/gobierto-etl-utils;
 
 # Extract > Download data sources
-cd $DEV_DIR/gobierto-etl-utils/; ruby operations/download-s3/run.rb "esplugues/budgets/$XBRL_FILE" /tmp/esplugues/
+cd $GOBIERTO_ETL_UTILS; ruby operations/download-s3/run.rb "esplugues/budgets/$XBRL_FILE" $WORKING_DIR/
 
-cd $DEV_DIR/gobierto-etl-utils/; ruby operations/gobierto_budgets/xbrl/trimloc/transform-execution/run.rb operations/gobierto_budgets/xbrl/dictionaries/xbrl_trimloc_dictionary.yml /tmp/esplugues/$XBRL_FILE 8077 2018 /tmp/esplugues/budgets-execution-2018.json
+cd $GOBIERTO_ETL_UTILS; ruby operations/gobierto_budgets/xbrl/trimloc/transform-execution/run.rb operations/gobierto_budgets/xbrl/dictionaries/xbrl_trimloc_dictionary.yml $WORKING_DIR/$XBRL_FILE 8077 2018 $WORKING_DIR/budgets-execution-2018.json
 
-cd $DEV_DIR/gobierto-etl-utils/; ruby operations/gobierto_budgets/import-executed-budgets/run.rb /tmp/esplugues/budgets-execution-2018.json 2018
+cd $GOBIERTO_ETL_UTILS; ruby operations/gobierto_budgets/import-executed-budgets/run.rb $WORKING_DIR/budgets-execution-2018.json 2018
 
 # Load > Calculate totals
-echo "8077" > /tmp/esplugues/organization.id.txt
-cd $DEV_DIR/gobierto-etl-utils/; ruby operations/gobierto_budgets/update_total_budget/run.rb "2018" /tmp/esplugues/organization.id.txt
+echo "8077" > $WORKING_DIR/organization.id.txt
+cd $GOBIERTO_ETL_UTILS; ruby operations/gobierto_budgets/update_total_budget/run.rb "2018" $WORKING_DIR/organization.id.txt
 
 # Load > Calculate bubbles
-cd $DEV_DIR/gobierto-etl-utils/; ruby operations/gobierto_budgets/bubbles/run.rb /tmp/esplugues/organization.id.txt
+cd $GOBIERTO_ETL_UTILS; ruby operations/gobierto_budgets/bubbles/run.rb $WORKING_DIR/organization.id.txt
 
 # Load > Calculate annual data
-cd $DEV_DIR/gobierto/; bin/rails runner $DEV_DIR/gobierto-etl-utils/operations/gobierto_budgets/annual_data/run.rb "2018" /tmp/esplugues/organization.id.txt
+cd $DEV_DIR/gobierto/; bin/rails runner $GOBIERTO_ETL_UTILS/operations/gobierto_budgets/annual_data/run.rb "2018" $WORKING_DIR/organization.id.txt
 
 # Load > Publish activity
-cd $DEV_DIR/gobierto/; bin/rails runner $DEV_DIR/gobierto-etl-utils/operations/gobierto/publish-activity/run.rb budgets_updated /tmp/esplugues/organization.id.txt
+cd $DEV_DIR/gobierto/; bin/rails runner $GOBIERTO_ETL_UTILS/operations/gobierto/publish-activity/run.rb budgets_updated $WORKING_DIR/organization.id.txt
