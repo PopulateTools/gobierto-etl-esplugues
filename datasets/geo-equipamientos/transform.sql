776cb87313a28ab85468e95edeb1e4ecfcf4208b
Fernando Blat <fernando@blat.es>
2023-02-17 07:19:57 +0100

    Use geom column name

--- datasets/geo-equipamientos/transform.sql
+++ datasets/geo-equipamientos/transform.sql
@@ -1,80 +1,80 @@
 DROP VIEW IF EXISTS data;
 CREATE VIEW data AS
-  SELECT FID as id 
+  SELECT FID as id
   ,nom as "equipment_name"
   ,tipus as "equipment_type"
   ,adreça || '. ' || cp_pobl as "location"
-  ,correu as "email" 
+  ,correu as "email"
   ,telefon as phone_number
-  ,geom as geometry
+  ,geom as geom
   FROM atencio_social_raw
-  
-UNION ALL 
-  SELECT FID as id 
+
+UNION ALL
+  SELECT FID as id
   ,nom as "equipment_name"
   ,tipus as "equipment_type"
   ,adreça || '. ' || cp_pobl as "location"
-  ,correu as "email" 
+  ,correu as "email"
   ,telefon as phone_number
-  ,geom as geometry 
+  ,geom as geom
   FROM cultura_raw
 
-UNION ALL 
-  SELECT FID as id 
+UNION ALL
+  SELECT FID as id
   ,nom as "equipment_name"
   ,tipus as "equipment_type"
   ,adreça || '. ' || cp_pobl as "location"
-  ,correu as "email" 
+  ,correu as "email"
   ,telefon as phone_number
-  ,geom as geometry
+  ,geom as geom
   FROM ensenyament_raw
 
-UNION ALL 
-  SELECT FID as id 
+UNION ALL
+  SELECT FID as id
   ,nom as "equipment_name"
   ,tipus as "equipment_type"
   ,adreça || '. ' || cp_pobl as "location"
-  ,correu as "email" 
+  ,correu as "email"
   ,telefon as phone_number
-  ,geom as geometry 
+  ,geom as geom
   FROM esplais_jubilats_raw
 
-UNION ALL 
-  SELECT FID as id 
+UNION ALL
+  SELECT FID as id
   ,nom as "equipment_name"
   ,tipus as "equipment_type"
   ,adreça || '. ' || cp_pobl as "location"
-  ,correu as "email" 
+  ,correu as "email"
   ,telefon as phone_number
-  ,geom as geometry 
+  ,geom as geom
   FROM esport_raw
 
-UNION ALL 
-  SELECT FID as id 
+UNION ALL
+  SELECT FID as id
   ,nom as "equipment_name"
   ,tipus as "equipment_type"
   ,adreça || '. ' || cp_pobl as "location"
-  ,correu as "email" 
+  ,correu as "email"
   ,telefon as phone_number
-  ,geom as geometry 
+  ,geom as geom
   FROM mercats_raw
 
-UNION ALL 
-  SELECT FID as id 
+UNION ALL
+  SELECT FID as id
   ,nom as "equipment_name"
   ,tipus as "equipment_type"
   ,adreça || '. ' || cp_pobl as "location"
-  ,correu as "email" 
+  ,correu as "email"
   ,telefon as phone_number
-  ,geom as geometry 
+  ,geom as geom
   FROM residencies_geriatriques_raw
 
-UNION ALL 
-  SELECT FID as id 
+UNION ALL
+  SELECT FID as id
   ,nom as "equipment_name"
   ,tipus as "equipment_type"
   ,adreça || '. ' || cp_pobl as "location"
-  ,correu as "email" 
+  ,correu as "email"
   ,telefon as phone_number
-  ,geom as geometry 
+  ,geom as geom
   FROM salut_raw;
